8b002d7b267edc86f6d590638812266c48ec5d83
miguelfdz <miguelfdzm08@gmail.com>
2021-05-25 21:06:39 -0500

    Added total sales column to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_234950) do
+ActiveRecord::Schema.define(version: 2021_05_26_015242) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_234950) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
     t.decimal "discount"
+    t.integer "total_sales"
   end
 
   create_table "users", force: :cascade do |t|
