d86e59c9f54f27f5b02b4c48d7fb60c2ec579b54
miguelfdz <miguelfdzm08@gmail.com>
2021-05-26 04:42:15 -0500

    Added price with discount to offers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  def self.price_with_discount(product)
+    (product.price - ((product.discount/100) * product.price)).round(2)
+  end
 end
