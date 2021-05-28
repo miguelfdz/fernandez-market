c8391778a54fcfd7cd89739c06e25b89e2010b8f
miguelfdz <miguelfdzm08@gmail.com>
2021-05-28 04:02:05 -0500

    las changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   has_one_attached :image
-  belongs_to :cart
 
   def self.price_with_discount(product)
     (product.price - ((product.discount/100) * product.price)).round(2)
